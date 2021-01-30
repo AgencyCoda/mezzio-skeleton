56d768bf926b4dc3ec553f56e3a5db8b432c8a45
Matias Camiletti <matias.camiletti@gmail.com>
2021-01-30 16:55:51 -0300

    add Api Key header allow.

--- config/autoload/cors.global.php
+++ config/autoload/cors.global.php
@@ -7,7 +7,7 @@ use \Mezzio\Cors\Configuration\ConfigurationInterface;
 return [
     ConfigurationInterface::CONFIGURATION_IDENTIFIER => [
         'allowed_origins' => [ConfigurationInterface::ANY_ORIGIN], // Allow any origin
-        'allowed_headers' => ["Authorization", "If-Match", "If-Unmodified-Since", "Content-Type", "Accept", "Origin", "X-Auth-Token", "X-Requested-With", "Access-Control-Allow-Headers", "Access-Control-Allow-Origin"], // No custom headers allowed
+        'allowed_headers' => ["Authorization", "If-Match", "If-Unmodified-Since", "Content-Type", "Accept", "Origin", "X-Auth-Token", "X-Requested-With", "Access-Control-Allow-Headers", "Access-Control-Allow-Origin", "X-Api-Key"], // No custom headers allowed
         'allowed_max_age' => '600', // 10 minutes
         'credentials_allowed' => true, // Allow cookies
         'exposed_headers' => ['X-Custom-Header', 'Etag'], // Tell client that the API will always return this header
