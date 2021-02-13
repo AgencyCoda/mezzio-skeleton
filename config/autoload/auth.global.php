0fb103298a6037afb30b0e329f498a23e87dae00
Matias Camiletti <matias.camiletti@gmail.com>
2021-02-13 07:45:36 -0300

    Fix get envs.

--- config/autoload/auth.global.php
+++ config/autoload/auth.global.php
@@ -2,7 +2,7 @@
 
 return [
     'mia_auth' => [
-        'key' => getenv('API_KEY') ?? '',
+        'key' => getenv('API_KEY') ? getenv('API_KEY') : 'KEYCODAHUB21PLATFORM',
         'method' => 'jwt',
         'iss' => 'https://agencycoda.com',
         'aud' => 'https://agencycoda.com',
--- config/autoload/eloquent.global.php
+++ config/autoload/eloquent.global.php
@@ -3,11 +3,11 @@
 return [
     'eloquent' => [
         'driver'    => 'mysql',
-        'host'      => getenv('DB_HOST') ?? 'localhost:8889',
-        'unix_socket' => getenv('DB_UNIXSOCKET') ?? null,
-        'database'  => getenv('DB_DATABASE') ?? 'coinsport',
-        'username'  => getenv('DB_USERNAME') ?? 'root',
-        'password'  => getenv('DB_PASSWORD') ?? 'root',
+        'host'      => getenv('DB_HOST') ? getenv('DB_HOST') : 'localhost:8889',
+        'unix_socket' => getenv('DB_UNIXSOCKET') ? getenv('DB_UNIXSOCKET') : null,
+        'database'  => getenv('DB_DATABASE') ? getenv('DB_DATABASE') : 'codahub',
+        'username'  => getenv('DB_USERNAME') ? getenv('DB_USERNAME') : 'root',
+        'password'  => getenv('DB_PASSWORD') ? getenv('DB_PASSWORD') : 'root',
         'charset'   => 'utf8',
         'collation' => 'utf8_general_ci',
     ]
